5451ae900a960094fdffed46362c4cb51c7e1c1c
pragita <pragitasuwal@gmail.com>
2016-09-21 16:38:54 +0545

    Merge branch 'master' of https://github.com/somitraj/venuebook
    # Conflicts:
    #	app/Http/Controllers/VenueController.php

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,14 @@ Route::get('/login',[
     'as'=>'web.login',
     'uses'=>'UserController@login'
 ]);
+Route::get('/Register',[
+    'as'=>'web.Register',
+    'uses'=>'UserController@Register'
+]);
+Route::get('/Address',[
+    'as'=>'web.Address',
+    'uses'=>'UserController@Address'
+]);
 Route::get('/Feedback',[
     'as'=>'web.Feedback',
     'uses'=>'FeedbackController@Feedback'
