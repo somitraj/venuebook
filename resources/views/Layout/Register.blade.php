c352ba966408f20932962409ddfd5e3418ca6d60
pragita <pragitasuwal@gmail.com>
2016-09-21 13:30:22 +0545

    changes

---
+++ resources/views/Layout/Register.blade.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> {{-- imp to make responsive--}}
+    <title>{{ trans('front/site.title') }}</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1"> {{--imp to make responsive--}}
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="jquery/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+
+
+    @yield('head')
+
+</head>
+
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-1">
+            <img src="images/logo.png" height="120" width="120">
+
+        </div>
+        <div class="col-md-11">
+            <img src="images/banner.jpg" >
+        </div>
+    </div>
+     <div class="navbar navbar-default">
+                      <div class="navbar-header">
+
+              </div>
+    {{--
+                    </div>--}}
+
+    <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Contact Us</a></li>
+        <li><a href="#">Feedback</a></li>
+        <li><a href="#">Register</a></li>
+        <li><a href="#">Login</a></li>
+    </ul>
+</div>
+
+    <div class="panel panel-primary">
+        <div class="panel-heading text-center" >Account Information</div>
+
+
+                        <div class="panel-body">Login Name*:
+                        <div class="panel-body">Password* :
+                        <div class="panel-body">Confirm Password* :
+                        <div class="panel-body">Email Address* :
+                        <div class="panel-body">Confirm Email* :
+          {{--  </fieldset>
+        </form>
+--}}
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
