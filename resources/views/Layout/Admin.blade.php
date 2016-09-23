7b5f1b5d9fde5ca96121059e1b8368f1ba7322c0
maskey12 <simranmaskey12@gmail.com>
2016-09-23 13:03:34 -0700

    adminlayout updated

---
+++ resources/views/Layout/Admin.blade.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hp
+ * Date: 9/21/2016
+ * Time: 12:51 PM
+ */
+?>
+@extends('Layout/AdminLayout')
+@section('menu')
+
+    <div class="row">
+        <div class="form-group col-lg-11">
+        </div>
+
+        <div class="form-group col-lg-1">
+            <div class="input-group">
+               {{-- <input type="text" class="form-control" id="logout">--}}
+
+                <span class="input-group-btn" ><button type="button" class="btn btn-primary">Logout</button></span>
+
+            </div>
+            </div>
+        </div>
+    <nav class="navbar navbar-default">
+        <div class="navbar-header">
+
+        </div>
+    <ul class="nav navbar-nav">
+        <li class="active" ><a href="/" class="glyphicon glyphicon-globe"> Notification</a></li>
+        <li><a href="/Client" class="glyphicon glyphicon-user"> Client</a></li>
+        <li><a href="/Venue" class="glyphicon glyphicon-group"> Venue</a></li>
+        <li><a href="/Settings" class="glyphicon glyphicon-cog"> Settings</a></li>
+
+
+
+    </ul>
+    </nav>
+    </body>
+    </html>
+
+@endsection
+
+
+
