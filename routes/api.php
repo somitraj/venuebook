90f4d987f26197a2b16f81b0a0f3805f5bfb2b81
Amangunner <cynite_mah88@hotmail.com>
2016-09-26 16:09:52 +0545

    Merge branch 'master' of https://github.com/somitraj/venuebook
    # Conflicts:
    #	app/User.php

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,3 @@ use Illuminate\Http\Request;
 //Route::get('/user', function (Request $request) {
 //    return $request->user();
 //})->middleware('auth:api');
-$api = app('Dingo\Api\Routing\Router');
-$api->version('v1',['namespace'="Venue\HTTP\controller"] function ($api) {
-
-});
