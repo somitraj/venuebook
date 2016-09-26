5917c62496481fbeea50753f425ebb58b5867bab
maskey12 <simranmaskey12@gmail.com>
2016-09-26 16:29:47 -0700

    hjjk

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Venue\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+
+use Venue\Http\Requests;
+use Venue\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    public function Register(Request $request){
+
+    }
+}
