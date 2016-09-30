57f2492b1f4b5e0fc29abfb0b30edb192067121c
maskey12 <simranmaskey12@gmail.com>
2016-09-30 12:37:11 -0700

    made

--- resources/views/Layout/Admin.blade.php
+++ resources/views/Layout/Admin.blade.php
@@ -15,19 +15,19 @@
         <li><a href="/Notification" class="glyphicon glyphicon-bell"> <span class="badge">0</span> Notification</a></li>
         <li><a href="/Client" class="glyphicon glyphicon-user"> Client</a></li>
         <li><a href="/Venue" class="glyphicon glyphicon-group"> Venue</a></li>
-        <li><a href="/Settings" class="glyphicon glyphicon-cog dropdown-toggle"> Settings
+        <li><a href="/Settings" class="glyphicon glyphicon-cog" data-toggle="dropdown"> Settings
+                <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="/Username">Username</a></li>
+                    <li><a href="/Password">Password</a></li>
+                </ul>
+</li>
 
-                <span class="caret"></span>
-
-                <ul class="dropdown-menu">/
-
-                <li><a href="#">Username</a></li>
-                <li><a href="#">Password</a></li>
-            </ul>
-            </a></li>
-        <button type="button" class="btn btn-primary" style="float: right margin:left ">
+</ul>
+        <button type="button" class="btn btn-primary" style="float: right margin:left;margin-left: 665px;">
             <span class="glyphicon glyphicon-log-out "></span>Logout
         </button>
+
 
             </div>
         </nav>
@@ -39,6 +39,8 @@
                             <div class="panel panel-primary">
                                 <div class="panel-heading ">
                                     <h6 style="text-align:center">NOTIFICATION</h6></div>
+
+
 
                                 <div class="panel-body"><img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
                             </div>
@@ -63,7 +65,7 @@
                     </div>
                     <div class="col-sm-4">
 
-                        <div class="panel panel-warning">
+                        <div class="panel panel-default">
                             <div class="panel-heading">
                                 <h6 style="text-align:center">SETTINGS</h6></div>
 
