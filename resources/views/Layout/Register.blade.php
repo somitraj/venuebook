e80fde11f653f8822a985d23a81c0c01257763c9
maskey12 <simranmaskey12@gmail.com>
2016-09-30 19:46:48 -0700

    register blade changed

--- resources/views/Layout/Register.blade.php
+++ resources/views/Layout/Register.blade.php
@@ -18,13 +18,7 @@
                 <li><a href="/" class="glyphicon glyphicon-home"> Home</a></li>
                 <li><a href="/Contact" class="glyphicon glyphicon-phone-alt"> Contact Us</a></li>
                 <li><a href="/Feedback" class="glyphicon glyphicon-tags"> Feedback</a></li>
-                <li class="dropdown active"><a href="/Register" class="dropdown-toggle glyphicon glyphicon-pencil" data-toggle="dropdown"> Register
-                            <span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="/Register">Client</a></li>
-                            <li><a href="/Venue">Venue</a></li>
-                            </ul>
-                          </li>
+                <li class="active"><a href="/Register" class="glyphicon glyphicon-tags"> Feedback</a></li>
                 <li><a href="/login" class="glyphicon glyphicon-hand-right"> Login</a></li>
             </ul>
         </div>
