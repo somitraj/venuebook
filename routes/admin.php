d1c50d8e5498492784abad6a27f056b334a082ab
maskey12 <simranmaskey12@gmail.com>
2016-09-30 19:47:32 -0700

    changed

--- routes/admin.php
+++ routes/admin.php
@@ -1,9 +1,11 @@
 <?php
-Route::group(["role"=>"1","prefix"=>"admin"],function()
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 9/30/2016
+ * Time: 2:54 PM
+ */
+Route::group(["role"=>1,"prefix"=>"admin"],function()
 {
-    Route::any("dashboard",["as"=>'admin.dashboard',
-                                "uses"=>'UserController@Admin']);
+    Route::any("dashboard",["as"=>'admin.dash', "uses"=>'UserController@Admin']);
 });
-
-
-?>
