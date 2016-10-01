1b83e4f8f91eb6a1858c3b7149a17f5364de3278
maskey12 <simranmaskey12@gmail.com>
2016-09-30 19:56:59 -0700

    some changes made

--- resources/views/Layout/Manager.blade.php
+++ resources/views/Layout/Manager.blade.php
@@ -14,7 +14,7 @@
             <ul class="nav navbar-nav">
                 <li><a href="/DashBoard" class="glyphicon glyphicon-dashboard"> Dashboard </a></li>
                 <li><a href="/MyAccount" class="glyphicon glyphicon-user"> MyAccount</a></li>
-                <li><a href="/Notification" class="glyphicon glyphicon-globe" ><button class="btn btn-primary" type="button">
+                <li><a href="/Notification" class="glyphicon glyphicon-globe" >
                         Notification <span class="badge">4</span>
                     </button></a></li>
                 <li class="dropdown">
@@ -27,7 +27,7 @@
                 <li><a href="/Feedback" class="glyphicon glyphicon-tags"> Feedback</a></li>
 
 
-    <button type="button" class="btn btn-default btn-md" style="float: right">
+    <button type="button" class="btn btn-default btn-md" style="float: right;margin-left: 578px;">
         <span class="glyphicon glyphicon-log-out" aria-hidden="true"></span> Log Out
     </button>
 
