3a9372be4e965cc2826453227127d0392c55ecd5
maskey12 <simranmaskey12@gmail.com>
2016-10-17 16:07:49 -0700

    changed

--- resources/views/Layout/Home.blade.php
+++ resources/views/Layout/Home.blade.php
@@ -102,10 +102,10 @@
                     <li><a class="ns-img" href="images/venue3.jpg"></a></li>
                     <li><a class="ns-img" href="images/venue4.jpg"></a></li>
                     <li><a class="ns-img" href="images/venue5.jpg"></a></li>
-                    <li><a class="ns-img" href="images/venue6.jpg"></a></li>
+                    <li><a class="ns-img" href="images/at.jpg"></a></li>
                     <li><a class="ns-img" href="images/Palagya Hotel.jpg"></a></li>
                     <li><a class="ns-img" href="images/Royal Food Cafe and Banquet.jpg"></a></li>
-                    <li><a class="ns-img" href="images/S.R Party palace.jpg"></a></li>
+                    <li><a class="ns-img" href="images/s.jpg"></a></li>
                     <li><a class="ns-img" href="images/venue1.jpg"></a></li>
                     <li><a class="ns-img" href="images/venue2.jpg"></a></li>
                     <li><a class="ns-img" href="images/venue3.jpg"></a></li>
@@ -132,7 +132,7 @@
                         <a class="thumb" href="images/venue5.jpg"></a>
                     </li>
                     <li>
-                        <a class="thumb" href="images/venue6.jpg"></a>
+                        <a class="thumb" href="images/at.jpg"></a>
                     </li>
                     <li>
                         <a class="thumb" href="images/Palagya Hotel.jpg"></a>
@@ -141,7 +141,7 @@
                         <a class="thumb" href="images/Royal Food Cafe and Banquet.jpg"></a>
                     </li>
                     <li>
-                        <a class="thumb" href="images/S.R Party palace.jpg"></a>
+                        <a class="thumb" href="images/s.jpg"></a>
                     </li>
                     <li>
                         <a class="thumb" href="images/venue1.jpg"></a>
