d9fdc3459f4c38de8d08d55c7af82eb02765eeb5
maskey12 <simranmaskey12@gmail.com>
2016-10-17 12:08:02 -0700

    some changes

--- app/Forms/VenueTypeForm.php
+++ app/Forms/VenueTypeForm.php
@@ -8,24 +8,24 @@ class VenueTypeForm extends Form  //also includes user type content
 {
     public function buildForm()
     {
-        $venue_types=$this->getFormOption('venue_type');
-        $venueOption=[];
+        $venue_types=$this->getFormOptions('venuetype');
+      // print_r($venue_types);die();
+        $venueOption = [];
         foreach($venue_types->venue_type as $venue_type)
         {
-            $venueOption[$venue_type->id]=$venue_type->type_name;
+                  $venueOption[$venue_type->id]=$venue_type->type_name;
         }
-        $this
+            $this
+                ->add('venue_type', 'select', [
+                        'choices' => $venueOption,
+                        /*  'selected' => 'en',*/
+                        'label' => 'Venue Type',
+                        'empty_value' => '=== Select Venue Type ===',
+                        'wrapper' => ['class' => 'form-group row'],
+                        'label_attr' => ['class' => 'col-md-3 control-label'],
+                        'attr' => ['class' => 'col-md-8 form-control field-input']
+                    ]
+                );
 
-            ->add('venue_type', 'select', [
-                    'choices' => $venueOption,
-                    /*  'selected' => 'en',*/
-                    'label'=>'Venue Type',
-                    'empty_value' => '=== Select Venue Type ===',
-                    'wrapper' =>['class' => 'form-group row'],
-                    'label_attr'=>['class'=>'col-md-3 control-label'],
-                    'attr' =>['class' => 'col-md-8 form-control field-input']
-                ]
-            );
-
+        }
     }
-}
