de5cd50e7fb23370ef9af9d1863ad0c4d2bb0f83
pragita <pragitasuwal@gmail.com>
2016-10-21 13:32:11 +0545

    commit

--- routes/admin.php
+++ routes/admin.php
@@ -5,12 +5,7 @@
  * Date: 9/30/2016
  * Time: 2:54 PM
  */
-Route::group(["role"=>1,'middleware'=>'auth.user',"prefix"=>"admin"],function()
-{                                                                               //yesko vitra admin le kun kun route jana milxa define garne
+Route::group(["role"=>1,"prefix"=>"admin"],function()
+{
     Route::any("dashboard",["as"=>'admin.dash', "uses"=>'UserController@Admin']);
-     Route::any("client",["as"=>'admin.client', "uses"=>'UserController@UserList']);
-    Route::any("venue",["as"=>'admin.venue', "uses"=>'UserController@ManagerList']);
-/*    Route::any("venue",["as"=>'admin.venue', "uses"=>'UserController@VenueList']);*/
-    Route::any("notification",["as"=>'admin.notice', "uses"=>'NotificationController@GetNotice']);
-    Route::any("detailview",["as"=>'admin.detailview', "uses"=>'UserController@GetDetailView']);
 });
--- routes/manager.php
+++ routes/manager.php
@@ -2,15 +2,10 @@
 /**
  * Created by PhpStorm.
  * User: Admin
- * Date: 9/30/2016bvhv
+ * Date: 9/30/2016
  * Time: 2:54 PM
  */
-Route::group(["role"=>2,"prefix"=>"manager",'middleware'=>'auth.user'],function()
-{//yesko vitra manager le kun kun route jana milxa define garne
+Route::group(["role"=>2,"prefix"=>"manager"],function()
+{
     Route::any("dashboard",["as"=>'manager.dash', "uses"=>'UserController@Manager']);
-    Route::any("feedback",["as"=>'manager.feedback', "uses"=>'FeedbackController@Feedback']);
-    /*Route::get('/searchlost',['as'=>'searchlost','uses'=>'Web\LoginController@Search']);
-    Route::get('/searchfound',['as'=>'searchfound','uses'=>'Web\LoginController@Lost']);
-    Route::get('/lostitem',['as'=>'lostitem','uses'=>'Web\LoginController@ReportLost']);
-    Route::get('/founditem',['as'=>'founditem','uses'=>'Web\LoginController@ReportFound']);*/
 });
