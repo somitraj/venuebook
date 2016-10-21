95e5c1528274cc9e17df815bfee1f41da44e8d57
somitraj <ranjitkarsomit@gmail.com>
2016-10-21 14:10:52 +0545

    Merge branch 'master' of https://github.com/somitraj/venuebook
    # Conflicts:
    #	routes/admin.php

--- app/Forms/BookingForm.php
+++ app/Forms/BookingForm.php
@@ -8,50 +8,31 @@ class BookingForm extends Form
 {
     public function buildForm()
     {
-      $this
-          ->add('cover','file', [
-                  'wrapper' => ['class' => 'form-group row'],
-                  'label'=>'Cover Image',
-                  'label_attr'=>['class'=>'col-md-3 control-label'],
-                  'attr' => ['class' => 'col-md-8 form-control field-input','accept'=>'.jpeg,.png,.jpg']
+        $this
+            ->add('Booking Date', 'date', [
+                    'wrapper' =>['class' => 'form-group row'],
+                    'label_attr'=>['class'=>'col-md-3 control-label'],
+                    'attr' =>['class' => 'col-md-9 form-control field-input']
+                ]
+            )
 
-              ]
-          )
 
-          ->add('image1','file', [
-                  'wrapper' => ['class' => 'form-group row'],
-                  'label'=>'Image1',
-                  'label_attr'=>['class'=>'col-md-3 control-label'],
-                  'attr' => ['class' => 'col-md-8 form-control field-input','accept'=>'.jpeg,.png,.jpg']
 
-              ]
-          )
+            ->add('Event','text', [
+                    'wrapper' => ['class' => 'form-group row'],
+                    'label_attr'=>['class'=>'col-md-3 control-label'],
+                    'attr' => ['class' => 'col-md-9 form-control field-input']
 
-          ->add('image2','file', [
-                  'wrapper' => ['class' => 'form-group row'],
-                  'label'=>'Image2',
-                  'label_attr'=>['class'=>'col-md-3 control-label'],
-                  'attr' => ['class' => 'col-md-8 form-control field-input','accept'=>'.jpeg,.png,.jpg']
+                ]
+            )
+            ->add('Booking time','time', [
+                    'wrapper' => ['class' => 'form-group row'],
+                    'label_attr'=>['class'=>'col-md-3 control-label'],
+                    'attr' => ['class' => 'col-md-9 form-control field-input']
 
-              ]
-          )
-          ->add('image3','file', [
-                  'wrapper' => ['class' => 'form-group row'],
-                  'label'=>'Image3',
-                  'label_attr'=>['class'=>'col-md-3 control-label'],
-                  'attr' => ['class' => 'col-md-8 form-control field-input','accept'=>'.jpeg,.png,.jpg']
+                ]
+            )
 
-              ]
-          )
-          ->add('image4','file', [
-                  'wrapper' => ['class' => 'form-group row'],
-                  'label'=>'Image4',
-                  'label_attr'=>['class'=>'col-md-3 control-label'],
-                  'attr' => ['class' => 'col-md-8 form-control field-input','accept'=>'.jpeg,.png,.jpg']
-
-              ]
-          )
-
-            ->add('Next','submit',['attr' =>['class'=> 'btn btn-primary btn-block']]);
+            ->add('next','submit',['attr' =>['class'=> 'btn btn-primary btn-block']]);
     }
 }
