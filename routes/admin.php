2f8b33d265a6151684913340bcc1177d17ee2734
maskey12 <simranmaskey12@gmail.com>
2016-10-21 11:58:19 -0700

    comment made

--- routes/admin.php
+++ routes/admin.php
@@ -6,10 +6,13 @@
  * Time: 2:54 PM
  */
 Route::group(["role"=>1,'middleware'=>'auth.user',"prefix"=>"admin"],function()
-{ //yesko vitra admin le kun kun route jana milxa define garne
+{                                                                               //yesko vitra admin le kun kun route jana milxa define garne
     Route::any("dashboard",["as"=>'admin.dash', "uses"=>'UserController@Admin']);
-    Route::any("client",["as"=>'admin.client', "uses"=>'UserController@GetIndex']);
-    Route::any("venue",["as"=>'admin.venue', "uses"=>'UserController@GetManagerList']);
+     Route::any("client",["as"=>'admin.client', "uses"=>'UserController@UserList']);
+    Route::any("venue",["as"=>'admin.venue', "uses"=>'UserController@ManagerList']);
+    Route::any("venue",["as"=>'admin.venue', "uses"=>'UserController@VenueList']);
+    Route::any("notification",["as"=>'admin.notice', "uses"=>'NotificationController@GetNotice']);
+
 
 
 });
