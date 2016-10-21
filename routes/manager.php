05fc972bf46d016cb80f837830a0ffa83d8843aa
maskey12 <simranmaskey12@gmail.com>
2016-10-21 14:38:43 -0700

    venuelist made

--- routes/manager.php
+++ routes/manager.php
@@ -2,14 +2,10 @@
 /**
  * Created by PhpStorm.
  * User: Admin
- * Date: 9/30/2016bvhv
+ * Date: 9/30/2016
  * Time: 2:54 PM
  */
-Route::group(["role"=>2,"prefix"=>"manager",'middleware'=>'auth.user'],function()
-{//yesko vitra manager le kun kun route jana milxa define garne
+Route::group(["role"=>2,"prefix"=>"manager"],function()
+{
     Route::any("dashboard",["as"=>'manager.dash', "uses"=>'UserController@Manager']);
-    /*Route::get('/searchlost',['as'=>'searchlost','uses'=>'Web\LoginController@Search']);
-    Route::get('/searchfound',['as'=>'searchfound','uses'=>'Web\LoginController@Lost']);
-    Route::get('/lostitem',['as'=>'lostitem','uses'=>'Web\LoginController@ReportLost']);
-    Route::get('/founditem',['as'=>'founditem','uses'=>'Web\LoginController@ReportFound']);*/
 });
