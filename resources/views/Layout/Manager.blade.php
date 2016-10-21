8a7a4167a34918e7583b6ca62ba77ee3c535e811
pragita <pragitasuwal@gmail.com>
2016-10-21 13:27:03 +0545

    commit

--- resources/views/Layout/Manager.blade.php
+++ resources/views/Layout/Manager.blade.php
@@ -27,10 +27,7 @@
                 <div class="panel panel-primary">
                     <div class="panel-heading ">
                         <h6 style="text-align:center">NOTIFICATION</h6></div>
-
-
-
-                    <div class="panel-body"><img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+                    <div class="panel-body"><a href="Notification" class="panel"> <img src="/images/notification.png" class="panel" style="width:100%" alt="Image"></a></div>
                 </div>
             </div>
             <div class="col-sm-4">
@@ -38,28 +35,32 @@
                     <div class="panel-heading">
                         <h6 style="text-align:center">INVENTORY</h6></div>
 
-                    <div class="panel-body"><img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+                    <div class="panel-body"><a href="feedback" class="panel"><img src="/images/inventory.png" class="panel" style="width:100%" alt="Image"></a></div>
                 </div>
             </div>
             <div class="col-sm-4">
                 <div class="panel panel-success">
                     <div class="panel-heading">
                         <h6 style="text-align:center">FEEDBACK</h6></div>
-
-                    </a>
-                    <div class="panel-body"><img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+                    <div class="panel-body"><a href="feedback" class="panel"><img src="/images/feedback.jpg" class="img-responsive" style="width:100%" alt="Image"></a></div>
                 </div>
             </div>
-        </div>
-        <div class="col-sm-4">
+    </div>
 
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h6 style="text-align:center">SETTINGS</h6></div>
-
-                <div class="panel-body"><img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
-            </div>
+    <div class="row">
+    <div class="col-sm-4">
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                <h6 style="text-align:center">GALLERY</h6></div>
+            <div class="panel-body"><a href="venue" class="panel"><img src="/images/gallery.png" class="img-responsive" style="width:100%" alt="Image"></a></div>
         </div>
     </div>
+    </div>
+    </div>
     <br>
+
+
 @endsection
+
+
+
