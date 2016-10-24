bc0980cb666efc39c22025e9794bad43a0461e00
pragita <pragitasuwal@gmail.com>
2016-10-24 17:04:33 +0545

    c

---
+++ app/Http/Controllers/Api/SliderController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Venue\Http\Controllers\api;
+
+use Illuminate\Http\Request;
+
+use Venue\Http\Requests;
+use Venue\Http\Controllers\Controller;
+use Venue\Models\Venue;
+
+class SliderController extends Controller
+{
+    public function GetSlider(Request $request){
+
+
+
+
+    }
