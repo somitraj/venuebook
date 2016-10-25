7ee9ef0ecd79c3d2589c1dc128cc3afed7a6b305
somitraj <ranjitkarsomit@gmail.com>
2016-10-25 17:44:55 +0545

    Merge branch 'master' of https://github.com/somitraj/venuebook
    # Conflicts:
    #	resources/views/Layout/Managerlist.blade.php

---
+++ app/Http/Controllers/InventoryController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Venue\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Venue\Http\Requests;
+use Venue\Http\Controllers\Controller;
+
+class InventoryController extends Controller
+{
+    //
+}
