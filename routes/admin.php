690482dfb133182ec5cf2b6ffbc0e12b7558eefa
somitraj <ranjitkarsomit@gmail.com>
2016-10-28 16:49:11 +0545

    Merge branch 'master' of https://github.com/somitraj/venuebook
    # Conflicts:
    #	resources/views/Layout/Managerlist.blade.php

--- routes/admin.php
+++ routes/admin.php
@@ -8,18 +8,4 @@
 Route::group(["role"=>1,"prefix"=>"admin"],function()
 {
     Route::any("dashboard",["as"=>'admin.dash', "uses"=>'UserController@Admin']);
-
-    Route::any("client",["as"=>'admin.client', "uses"=>'UserController@UserList']);
-    Route::any("clientregister",["as"=>'admin.clientlist', "uses"=>'UserController@GetUserDetails']);
-    Route::any("venue",["as"=>'admin.venue', "uses"=>'UserController@ManagerList']);
-    /*    Route::any("venue",["as"=>'admin.venue', "uses"=>'UserController@VenueList']);*/
-    Route::any("notice",["as"=>'admin.notice', "uses"=>'NotificationController@GetNotice']);
-    //Route::any("venue",["as"=>'admin.venuelist', "uses"=>'VenueController@VenueList']);
-    Route::any("managerlist",["as"=>'admin.managerlist', "uses"=>'VenueController@Register']);
-    //Route::any("venuelist",["as"=>'admin.venuelist', "uses"=>'VenueController@VenueList']);
-    Route::any("venueregister",["as"=>'admin.venueregister', "uses"=>'VenueController@Register']);
-    Route::any('account',['as'=>'admin.account','uses'=>'UserController@UserAccount'  ]);
-
-
-
 });
--- routes/manager.php
+++ routes/manager.php
@@ -8,9 +8,4 @@
 Route::group(["role"=>2,"prefix"=>"manager"],function()
 {
     Route::any("dashboard",["as"=>'manager.dash', "uses"=>'UserController@Manager']);
-    Route::any("feedback",["as"=>'manager.feedback', "uses"=>'FeedbackController@Feedback']);
-    Route::any("gallery",["as"=>'manager.gallery', "uses"=>'GalleryController@Gallery']);
-    Route::any("inventory",["as"=>'manager.inventory', "uses"=>'InventoryController@Inventory']);
-    Route::any("account",["as"=>'manager.account', "uses"=>'UserController@ManagerVenuePage']);
-
 });
