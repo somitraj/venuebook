c1812a06e796aecf9acc97ccd9141225a3988004
somitraj <ranjitkarsomit@gmail.com>
2016-11-09 22:33:11 +0545

    Merge branch 'master' of https://github.com/somitraj/venuebook
    # Conflicts:
    #	routes/admin.php

--- resources/views/Layout/Userlist.blade.php
+++ resources/views/Layout/Userlist.blade.php
@@ -57,7 +57,7 @@ foreach($proimage as $pro){
                                @endif
                             </div>--}}
                                </div>
-                               @endif</div>
+
                    <table id="example" class="display table table-responsive table-striped" cellspacing="0" width="100%">
                     <thead>
                     <tr>
