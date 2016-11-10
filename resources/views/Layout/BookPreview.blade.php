cdc7e3167dedb94b738a083642032cafc8539669
somitraj <ranjitkarsomit@gmail.com>
2016-11-10 11:18:47 +0545

    Merge branch 'master' of https://github.com/somitraj/venuebook
    # Conflicts:
    #	routes/admin.php

--- resources/views/Layout/BookPreview.blade.php
+++ resources/views/Layout/BookPreview.blade.php
@@ -20,5 +20,12 @@ $total=$q*$p;
 @extends('Layout/MainLayout')
 
 @section('content')
+    <div class="col-lg-12">
+        <ol class="breadcrumb">
+            <li class="active">
+                <span class="glyphicon glyphicon-backward"></span> <a href="javascript:history.back()">Go Back To Booking Area</a>
+            </li>
+        </ol>
+    </div>
 
     @endsection
